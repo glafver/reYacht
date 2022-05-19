57763cc8d17912fd7550ff18246f8dc4caf2c5c8
Julia Nemell <jnemell@gmail.com>
2022-05-19 11:34:43 +0200

    results box with changable messages added to gameboard

--- src/components/Gameboards.jsx
+++ src/components/Gameboards.jsx
@@ -1,20 +1,25 @@
 import './css-components/Gameboards.css'
+import Results from './Results'
+
 
 const Gameboards = () => {
 
 
 	return (
 		<>
-			<div className="board-container">
-				<h1>My board</h1>
-				<div className="board player-grid">
+			<div className="boards">
+				<div className="board-container">
+					<h1>My board</h1>
+					<div className="board player-grid">
+					</div>
+				</div>
+				<div className="board-container">
+					<h1>Enemy board</h1>
+					<div className="board enemy-grid">
+					</div>
 				</div>
 			</div>
-			<div className="board-container">
-				<h1>Enemy board</h1>
-				<div className="board enemy-grid">
-				</div>
-			</div>
+			<Results />
 		</>
 	)
 }
