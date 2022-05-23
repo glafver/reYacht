29e37f8577f6960ec4f0f63d810af8e5d2fe887a
glafver <glafver22@gmail.com>
2022-05-23 13:41:45 +0200

    Allow to click on the enemy field just that user who can shoot now

--- src/components/Gameboards.jsx
+++ src/components/Gameboards.jsx
@@ -8,12 +8,16 @@ const Gameboards = () => {
 	const { userName, opponentName, yachts, shootTarget, move, setShootTarget } = useGameContext()
 
 	useEffect(() => {
+
 		const update = (e) => {
-			setShootTarget({ row: Math.ceil(e.offsetY / 30), col: Math.ceil(e.offsetX / 30) })
+			if (move && e.target.classList.contains('enemy-grid')) {
+				console.log('press')
+				setShootTarget({ row: Math.ceil(e.offsetY / 30), col: Math.ceil(e.offsetX / 30) })
+			}
 		}
 		window.addEventListener('click', update)
 
-	}, [setShootTarget])
+	}, [setShootTarget, move])
 
 	return (
 		<>
@@ -23,7 +27,7 @@ const Gameboards = () => {
 
 				<div className="board-container text-center">
 					<h1>{userName}</h1>
-					<div className="board player-grid m-auto">
+					<div className="board player-grid m-auto" >
 
 						{yachts && yachts.map(yacht =>
 							<div key={`${yacht.row_start} ${yacht.col_start}`} style={{ gridArea: (yacht.row_start + 1) + "/" + (yacht.col_start + 1) + "/" + yacht.row_end + "/" + yacht.col_end, backgroundColor: "green" }}></div>
@@ -32,7 +36,7 @@ const Gameboards = () => {
 				</div>
 				<div className="board-container text-center">
 					<h1>{opponentName}</h1>
-					<div className="board enemy-grid m-auto">
+					<div className="board enemy-grid m-auto" style={{ cursor: move === true ? "pointer" : "not-allowed" }}>
 					</div>
 				</div>
 			</div>
